cac0271ae700aa00c0de92bae6ef8bb85d21f539
Vatsal <vatsalpatel1073@gmail.com>
2020-12-29 19:18:05 +0530

    Intitated the routes for loggin in

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const User = require("../model/User");
 const { registerValidation, loginValidation } = require("../routes/validation");
+const bcrypt = require("bcryptjs");
 
+// register user
 router.post("/register", async (req, res) => {
   // validating the data
   const { error } = registerValidation(req.body);
@@ -9,17 +11,49 @@ router.post("/register", async (req, res) => {
     return res.status(400).send(error.details[0].message);
   }
 
+  // check for duplicate user
+  const emailExists = await User.findOne({ email: req.body.email });
+
+  if (emailExists) return res.status(400).send("email already exists");
+
+  // Hashing the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+
+  // create new user
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashedPassword,
   });
   try {
     const saveUser = await user.save();
-    res.status(201).send(saveUser);
+    res.status(201).send({ user: user._id });
   } catch (err) {
     res.status(400).send(err);
   }
+});
+
+// Log In
+
+router.post("/login", async (req, res) => {
+  // validating the data
+  const { error } = loginValidation(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // check for duplicate user
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) return res.send("register");
+
+  // check if password is correct
+  const validPass = await bcrypt.compare(req.body.password, user.password);
+  if (!validPass) return res.status(400).send("pass is wrong");
+
+  // jwt token
+
+  
 });
 
 module.exports = router;
